966f12a92e98ad4fd31d6cb70b9dce98261fa383
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-11-29 11:51:28 -0800

    add output plugins and modules in webpack config

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,4 +1,7 @@
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// Require the GenerateSW class of the WorkBoxPlugin 
+const WorkboxPlugin = require('workbox-webpack-plugin');
 const path = require('path');
 
 module.exports = () => {
@@ -12,16 +15,42 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
 
     },
     // TODO: Add the correct plugins
     plugins: [
-
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new MiniCssExtractPlugin(),
+      new WorkboxPlugin.GenerateSW()
     ],
 
     // TODO: Add the correct modules
     module: {
-
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
     }
   };
 };
