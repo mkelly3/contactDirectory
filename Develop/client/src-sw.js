583c36f39905b3e9c23df5bd9f8f2585f70df997
Morgan Kelly <mkelly5380@gmail.com>
2022-11-29 14:01:16 -0600

    added server file

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,1 +1,14 @@
 // TODO: Create a service worker that caches static assets:
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('./service-worker.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+  
+  this.addEventListener('fetch', function (event) {
+  });
+  
